9c560ed237d0b5247d01ac417c664073188dce17
Hero <icraft2170@gmail.com>
2022-11-26 22:01:26 +0900

    인프런 자바 알고리즘 강의 2번 문제

---
+++ java-algorithm-inflearn/src/main/java/me/hero/p2/Main.java
@@ -0,0 +1,25 @@
+package me.hero.p2;
+
+import java.util.Scanner;
+
+public class Main {
+  public String solution(String str) {
+    StringBuilder builder = new StringBuilder();
+    for (char ch : str.toCharArray()) {
+      if (Character.isUpperCase(ch)) {
+        builder.append(Character.toLowerCase(ch));
+      } else {
+        builder.append(Character.toUpperCase(ch));
+      }
+    }
+    return builder.toString();
+  }
+
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    String str = reader.next();
+
+    Main main = new Main();
+    System.out.print(main.solution(str));
+  }
+}
