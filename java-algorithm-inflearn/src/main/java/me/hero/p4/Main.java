6fe0b64fa080a061f237a34f9f645bb52692a392
Hero <icraft2170@gmail.com>
2022-11-28 23:07:04 +0900

    인프런 자바 알고리즘 강의 4번 문제 V2

--- java-algorithm-inflearn/src/main/java/me/hero/p4/Main.java
+++ java-algorithm-inflearn/src/main/java/me/hero/p4/Main.java
@@ -1,12 +1,22 @@
 package me.hero.p4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
-  public void solution(String[] strArray) {
-    for (String str : strArray) {
-      StringBuilder reverse = new StringBuilder(str).reverse();
-      System.out.println(reverse);
+  public String solution(String str) {
+    String answer;
+    char[] chars = str.toCharArray();
+    int lt = 0;
+    int rt = str.length() - 1;
+    while (lt < rt) {
+      char tmp;
+      tmp = chars[lt];
+      chars[lt] = chars[rt];
+      chars[rt] = tmp;
+      lt++;
+      rt--;
     }
+    return String.valueOf(chars);
   }
 
   public static void main(String[] args) {
@@ -19,6 +29,30 @@ public class Main {
     }
 
     Main main = new Main();
-    main.solution(strArray);
+    for (String str : strArray) {
+      System.out.println(main.solution(str));
+    }
   }
 }
+
+//public class Main {
+//  public void solution(String[] strArray) {
+//    for (String str : strArray) {
+//      StringBuilder reverse = new StringBuilder(str).reverse();
+//      System.out.println(reverse);
+//    }
+//  }
+//
+//  public static void main(String[] args) {
+//    Scanner reader = new Scanner(System.in);
+//    int index = reader.nextInt();
+//    reader.nextLine();
+//    String[] strArray = new String[index];
+//    for (int i = 0; i < index; i++) {
+//      strArray[i] = reader.nextLine();
+//    }
+//
+//    Main main = new Main();
+//    main.solution(strArray);
+//  }
+//}
