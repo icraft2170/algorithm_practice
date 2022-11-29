cb960fee4d4d58d037b9fd1083558833af7dcc33
Hero <icraft2170@gmail.com>
2022-11-29 22:40:53 +0900

    인프런 문제 5번

---
+++ java-algorithm-inflearn/src/main/java/me/hero/p5/Main.java
@@ -0,0 +1,29 @@
+package me.hero.p5;
+
+import java.util.Scanner;
+
+public class Main {
+  public String solution(String str) {
+    char[] chars = str.toCharArray();
+    int lt = 0;
+    int rt = chars.length - 1;
+    while (lt < rt) {
+      if (Character.isAlphabetic(chars[lt]) && Character.isAlphabetic(chars[rt])) {
+        char tmp = chars[lt];
+        chars[lt] = chars[rt];
+        chars[rt] = tmp;
+      }
+      lt++;
+      rt--;
+    }
+    return String.valueOf(chars);
+  }
+
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    String str = reader.nextLine();
+    Main main = new Main();
+    String result = main.solution(str);
+    System.out.print(result);
+  }
+}
