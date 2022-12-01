2e3643362e73b4b72fe60341b8c2bd578c9baf1b
Hero <icraft2170@gmail.com>
2022-12-01 23:17:22 +0900

    인프런 자바 알고리즘 강의 8번 문제

--- java-algorithm-inflearn/src/main/java/me/hero/p8/Main.java
+++ java-algorithm-inflearn/src/main/java/me/hero/p8/Main.java
@@ -1,11 +1,27 @@
 package me.hero.p8;
 
-public class Main {
-  public void solution() {
+import java.util.Scanner;
 
+public class Main {
+  public String solution(String str) {
+    str = str.toLowerCase();
+    str = str.replaceAll("[^a-z]", "");
+    String tmp = new StringBuilder(str).reverse().toString();
+
+    if (str.equals(tmp)) {
+      return "YES";
+    }
+    else {
+      return "NO";
+    }
   }
 
   public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    String str = reader.nextLine();
 
+    Main main = new Main();
+    String solution = main.solution(str);
+    System.out.println(solution);
   }
 }
