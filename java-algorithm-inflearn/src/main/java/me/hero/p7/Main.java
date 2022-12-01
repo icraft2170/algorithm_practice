cbeb03c760b84014ccc0479054764b146e29d9e6
Hero <icraft2170@gmail.com>
2022-12-01 23:07:04 +0900

    인프런 자바 알고리즘 강의 7번 문제 v2

--- java-algorithm-inflearn/src/main/java/me/hero/p7/Main.java
+++ java-algorithm-inflearn/src/main/java/me/hero/p7/Main.java
@@ -2,32 +2,54 @@ package me.hero.p7;
 
 import java.util.Scanner;
 public class Main {
-  public boolean solution(String str) {
-    char[] chars = str.toLowerCase().toCharArray();
-    boolean result = true;
-    int lt = 0;
-    int rt = chars.length - 1;
+  public String solution(String str) {
+    str = str.toLowerCase();
+    int length = str.length();
 
-    while (lt < rt) {
-      if (chars[lt] != chars[rt]) {
-        result = false;
+    for (int i = 0; i < length / 2; i++) {
+      if (str.charAt(i) != str.charAt(length - i - 1)) {
+        return "NO";
       }
-      lt++;
-      rt--;
     }
-    return result;
+    return "YES";
   }
 
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
     String str = reader.nextLine();
     Main main = new Main();
-    boolean result = main.solution(str);
-
-    if (result) {
-      System.out.print("YES");
-    } else {
-      System.out.print("NO");
-    }
+    System.out.print(main.solution(str));
   }
 }
+
+//
+//public class Main {
+//  public boolean solution(String str) {
+//    char[] chars = str.toLowerCase().toCharArray();
+//    boolean result = true;
+//    int lt = 0;
+//    int rt = chars.length - 1;
+//
+//    while (lt < rt) {
+//      if (chars[lt] != chars[rt]) {
+//        result = false;
+//      }
+//      lt++;
+//      rt--;
+//    }
+//    return result;
+//  }
+//
+//  public static void main(String[] args) {
+//    Scanner reader = new Scanner(System.in);
+//    String str = reader.nextLine();
+//    Main main = new Main();
+//    boolean result = main.solution(str);
+//
+//    if (result) {
+//      System.out.print("YES");
+//    } else {
+//      System.out.print("NO");
+//    }
+//  }
+//}
---
+++ java-algorithm-inflearn/src/main/java/me/hero/p8/Main.java
@@ -0,0 +1,11 @@
+package me.hero.p8;
+
+public class Main {
+  public void solution() {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
