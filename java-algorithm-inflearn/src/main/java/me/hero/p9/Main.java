c01d823e3f8f3023f450606c9902ef72a9d49c37
Hero <icraft2170@gmail.com>
2022-12-01 23:25:19 +0900

    인프런 자바 알고리즘 강의 9번 문제

--- java-algorithm-inflearn/src/main/java/me/hero/p9/Main.java
+++ java-algorithm-inflearn/src/main/java/me/hero/p9/Main.java
@@ -1,2 +1,26 @@
 package me.hero.p9;
 
+import java.util.Scanner;
+
+public class Main {
+  public int solution(String str) {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < str.length(); i++) {
+      char codePoint = str.charAt(i);
+      if (!Character.isAlphabetic(codePoint)) {
+        builder.append(codePoint);
+      }
+    }
+
+    return Integer.parseInt(builder.toString());
+  }
+
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    String str = reader.nextLine();
+
+    Main main = new Main();
+    int result = main.solution(str);
+    System.out.print(result);
+  }
+}
