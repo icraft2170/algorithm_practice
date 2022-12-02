171da830cef09a8e1f5a867c213dc3a127203e6b
Hero <icraft2170@gmail.com>
2022-12-02 23:34:33 +0900

    인프런 자바 알고리즘 강의 10번 문제V2

--- java-algorithm-inflearn/src/main/java/me/hero/p10/Main.java
+++ java-algorithm-inflearn/src/main/java/me/hero/p10/Main.java
@@ -6,43 +6,31 @@ public class Main {
   public void solution(String str) {
     String[] strArray = str.split(" ");
     String st = strArray[0];
-    int[] numbers1 = new int[st.length()];
-    int[] numbers2 = new int[st.length()];
     int[] answer = new int[st.length()];
-    char ch = strArray[1].charAt(0);
+    char t = strArray[1].charAt(0);
     StringBuilder result = new StringBuilder();
 
+    int distance = 1000;
     for (int i = 0; i < st.length(); i++) {
-      numbers1[i] = 0;
-      numbers2[i] = 0;
-      answer[i] = 0;
-    }
-
-    int count = 0;
-    for (int i = 0; i < st.length(); i++) {
-      if (st.charAt(i) == ch) {
-        count = 0;
+      if (st.charAt(i) == t) {
+        distance = 0;
+        answer[i] = distance;
       } else {
-        numbers1[i] = ++count;
+        ++distance;
+        answer[i] = distance;
       }
     }
 
-
+    distance = 1000;
     for (int i = st.length() - 1; i >= 0; i--) {
-      if (st.charAt(i) == ch) {
-        count = 0;
+      if (st.charAt(i) == t) {
+        distance = 0;
       } else {
-        numbers2[i] = ++count;
+        ++distance;
+        answer[i] = Math.min(distance, answer[i]);
       }
     }
 
-    for (int i = 0; i < st.length(); i++) {
-      if (numbers1[i] < numbers2[i]) {
-        answer[i] = numbers1[i];
-      } else {
-        answer[i] = numbers2[i];
-      }
-    }
 
     for (int i : answer) {
       result.append(i).append(" ");
