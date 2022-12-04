a564d8a701639ced0aa57779dbcfa494d4063dba
Hero <icraft2170@gmail.com>
2022-12-05 01:18:47 +0900

    인프런 알고리즘 P11 V2

--- java-algorithm-inflearn/src/main/java/me/hero/p11/Main.java
+++ java-algorithm-inflearn/src/main/java/me/hero/p11/Main.java
@@ -5,29 +5,18 @@ import java.util.Scanner;
 public class Main {
   public String solution(String str) {
     StringBuilder answer = new StringBuilder();
+    str = str + " ";
     char[] chars = str.toCharArray();
     int count = 1;
-    char tmp = ' ';
 
-    for (int i = 0; i < chars.length; i++) {
-      tmp = chars[i];
-      if ((chars.length - 1) == i) {
-        if (count == 1) {
-          answer.append(chars[chars.length - 1]);
-        } else {
-          answer.append(chars[chars.length - 1]).append(count);
-        }
-        break;
-      }
-
+    for (int i = 0; i < chars.length - 1; i++) {
       if (chars[i] == chars[i + 1]) {
         count++;
       } else {
-        if (count == 1) {
-          answer.append(tmp);
-          continue;
+        answer.append(chars[i]);
+        if (count > 1) {
+          answer.append(count);
         }
-        answer.append(tmp).append(count);
         count = 1;
       }
     }
