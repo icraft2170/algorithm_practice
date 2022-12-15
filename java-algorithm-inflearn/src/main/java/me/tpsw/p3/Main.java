7091221e6b9fa99a5c41a9abdf789d2332096dae
Hero <icraft2170@gmail.com>
2022-12-15 22:19:40 +0900

    인프런 two pointer algorithm p3 V2

--- java-algorithm-inflearn/src/main/java/me/tpsw/p3/Main.java
+++ java-algorithm-inflearn/src/main/java/me/tpsw/p3/Main.java
@@ -2,14 +2,21 @@ package me.tpsw.p3;
 
 import java.util.Scanner;
 
+/**
+ * Sliding Window 기법
+ */
 public class Main {
   public int solution(int n, int k, int[] arr) {
-    int max = Integer.MIN_VALUE;
-    for (int i = 0; i < n; i++) {
-      int sum = 0;
-      for (int j = 0; j < k; j++) {
-        if (i < n - j){ sum += arr[i + j];}
-      }
+    int max;
+    int sum = 0;
+
+    for (int i = 0; i < k; i++) {
+      sum += arr[i];
+    }
+    max = sum;
+
+    for (int i = k; i < n; i++) {
+      sum = sum - arr[i - k] + arr[i];
       max = Math.max(max, sum);
     }
     return max;
